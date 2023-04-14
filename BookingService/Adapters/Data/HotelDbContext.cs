bd1072c00940aff4534d21400806f0dd35840266
Douglas Basilio <basilio.douglas@yahoo.com.br>
2023-04-14 11:52:36 -0300

    subindo microservico BookingService utilizando DDD

---
+++ BookingService/Adapters/Data/HotelDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities = Domain.Entities;
+
+namespace Data
+{
+    public class HotelDbContext : DbContext
+    {
+        // injetando a parte de conexão com banco de dados
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
+
+        public virtual DbSet<Entities.Guest> Guests { get; set; }
+    }
+}
---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenace { get; set; }
+        public bool IsAvailable
+        {
+            get
+            {
+                if (this.InMaintenace || this.HasGuest)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        public bool HasGuest
+        {
+            // Verificar se tem bookings abertos para esta Room
+            get { return true; }
+        }
+    }
+}
